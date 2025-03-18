f27a294caae7490353defa588724172cdd3cbe8c
MounicaNaragani <mounica@affosoft.com>
2025-03-18 22:33:29 +0530

    Modified the Appointment book hook response

--- appointment-book.py
+++ appointment-book.py
@@ -49,71 +49,132 @@ def create_stub(response_body):
 
 
 def main():
-
     response_body = {
-        "cards": [
+        "cards": [],
+        "systemActions": [
             {
-                "summary": "Pre-appointment reminders",
-                "detail": "The patient is due for a flu shot. Recommend scheduling an immunization at the same visit.",
-                "indicator": "info",
-                "source": {
-                    "label": "Immunization Reminder Service",
-                    "url": "https://ehr.example.com/cds-services/immunization"
-                },
-                "suggestions": [
-                    {
-                        "label": "Schedule flu shot",
-                        "actions": [
-                            {
-                                "type": "create",
-                                "resource": {
-                                    "resourceType": "ServiceRequest",
-                                    "status": "active",
-                                    "intent": "order",
-                                    "code": {
-                                        "coding": [
-                                            {
-                                                "system": "http://snomed.info/sct",
-                                                "code": "86198006",
-                                                "display": "Influenza vaccination"
-                                            }
-                                        ]
-                                    },
-                                    "subject": {
-                                        "reference": "{{jsonPath request.body '$.context.patientId'}}"
-                                    }
-                                }
-                            }
-                        ]
-                    }
-                ]
+                "description": "Update to your resource",
+                "resourceId": "{{jsonPath request.body '$.context.appointments[0].id'}}",
+                "type": "update"
+            }
+        ]
+    }
+    response_body["systemActions"][0]["resource"] = {
+        "id": "{{jsonPath request.body '$.context.appointments[0].id'}}",
+        "appointmentType": {
+            "coding": [
+                {
+                    "code": "{{jsonPath request.body '$.context.appointments[0].appointmentType.coding[0].code'}}",
+                    "display": "{{jsonPath request.body '$.context.appointments[0].appointmentType.coding[0].display'}}",
+                    "system": "{{jsonPath request.body '$.context.appointments[0].appointmentType.coding[0].system'}}"
+                }
+            ],
+            "text": "{{jsonPath request.body '$.context.appointments[0].appointmentType.text'}}"
+        },
+        "basedOn": [
+            {
+                "reference": "{{jsonPath request.body '$.context.appointments[0].basedOn[0].reference'}}"
             }
         ],
-        "systemActions": [
-
-                            {
-                                "description": "create the resource",
-                                "type": "create",
-                                "resource": {
-                                    "resourceType": "ServiceRequest",
-                                    "status": "active",
-                                    "intent": "order",
-                                    "code": {
-                                        "coding": [
-                                            {
-                                                "system": "http://snomed.info/sct",
-                                                "code": "2731000",
-                                                "display": "Serologic test for Influenza A virus (procedure)"
-                                            }
-                                        ]
-                                    },
-                                    "subject": {
-                                        "reference": "{{jsonPath request.body '$.context.patientId'}}"
-                                    }
-                                }
-                            }
-                        ]
+        "description": "{{jsonPath request.body '$.context.appointments[0].description'}}",
+        "start": "{{jsonPath request.body '$.context.appointments[0].start'}}",
+        "end": "{{jsonPath request.body '$.context.appointments[0].end'}}",
+        # "participant": "{{jsonPath request.body '$.context.appointments[0].participant'}}",
+        # "speciality":"{{jsonPath request.body '$.context.appointments[0].speciality'}}",
+        "participant": [
+            {
+                "actor": {
+                    "reference": "{{jsonPath request.body '$.context.appointments[0].participant[0].actor.reference'}}"
+                },
+                "required": "{{jsonPath request.body '$.context.appointments[0].participant[0].required'}}",
+                "status": "{{jsonPath request.body '$.context.appointments[0].participant[0].status'}}"
+            },
+            {
+                "actor": {
+                    "reference": "{{jsonPath request.body '$.context.appointments[0].participant[1].actor.reference'}}"
+                },
+                "required": "{{jsonPath request.body '$.context.appointments[0].participant[1].required'}}",
+                "status": "{{jsonPath request.body '$.context.appointments[0].participant[1].status'}}"
+            }
+        ],
+        "status": "{{jsonPath request.body '$.context.appointments[0].status'}}",
+        "resourceType": "{{jsonPath request.body '$.context.appointments[0].resourceType'}}"
     }
+
+    response_body["systemActions"][0]["resource"]["extension"] = [
+        {
+            "extension": [
+                {
+                    "url": "coverageInfo",
+                    "valueCoding": {
+                        "code": "covered-prior-auth",
+                        "display": "Covered with prior authorization",
+                        "system": "http://hl7.org/fhir/us/davinci-crd/CodeSystem/temp"
+                    }
+                },
+                {
+                    "url": "questionnaire",
+                    "valueCanonical": "https://fhir-dev.mettles.com/baseServer/fhir/Questionnaire/questionnaire-lcd1-eyelidsurgery"
+                },
+                {
+                    "url": "coverage",
+                    "valueReference": {
+                        "reference": 'Coverage/'"{{jsonPath request.body '$.prefetch.coverageBundle.entry[0].resource.id'}}"
+                    }
+                },
+                {
+                    "url": "date",
+                    "valueDate": "2024-05-14"
+                },
+                {
+                    "url": "identifier",
+                    "valueString": "263adc54-ecf6-41b4-8ac8-055d0068c3fd"
+                },
+                {
+                    "url": "pa-needed",
+                    "valueCode": "auth-needed"
+                },
+                {
+                    "url": "covered",
+                    "valueCode": "covered"
+                },
+                {
+                    "url": "doc-needed",
+                    "valueCode": "clinical"
+                }
+            ],
+            "url": "http://hl7.org/fhir/us/davinci-crd/StructureDefinition/ext-coverage-information"
+        }
+    ]
+
+    # response_body = {
+    #     "cards": [],
+    #     "systemActions": [
+
+    #                         {
+    #                             "description": "create the resource",
+    #                             "type": "create",
+    #                             "resource": {
+    #                                 "resourceType": "ServiceRequest",
+    #                                 "status": "active",
+    #                                 "intent": "order",
+    #                                 "code": {
+    #                                     "coding": [
+    #                                         {
+    #                                             "system": "http://snomed.info/sct",
+    #                                             "code": "2731000",
+    #                                             "display": "Serologic test for Influenza A virus (procedure)"
+    #                                         }
+    #                                     ]
+    #                                 },
+    #                                 "subject": {
+    #                                     "reference": "{{jsonPath request.body '$.context.patientId'}}"
+    #                                 }
+    #                             }
+    #                         }
+    #                     ]
+    # }
+
     create_stub(response_body)
 
 
